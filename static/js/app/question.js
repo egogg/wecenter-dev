f5fac94bc25f1941a9e69795b32302c175623242
dengxiaodi <dixiaodeng@gmail.com>
2016-01-02 20:09:36 +0800

    add spinner before loading question

--- static/js/app/question.js
+++ static/js/app/question.js
@@ -126,6 +126,24 @@ $(function(){
 	// 加载问题内容
 
 	function loadQuestionContent() {
+		// 加载动画
+
+		var spiner = '<div class="sk-circle">' +
+			'<div class="sk-circle1 sk-child"></div>' +
+			'<div class="sk-circle2 sk-child"></div>' +
+			'<div class="sk-circle3 sk-child"></div>' +
+			'<div class="sk-circle4 sk-child"></div>' +
+			'<div class="sk-circle5 sk-child"></div>' +
+			'<div class="sk-circle6 sk-child"></div>' +
+			'<div class="sk-circle7 sk-child"></div>' +
+			'<div class="sk-circle8 sk-child"></div>' +
+			'<div class="sk-circle9 sk-child"></div>' +
+			'<div class="sk-circle10 sk-child"></div>' +
+			'<div class="sk-circle11 sk-child"></div>' +
+			'<div class="sk-circle12 sk-child"></div>' +
+			'</div>';
+		$('.question-loader').html(spiner);
+
 		$.get(G_BASE_URL + '/question/ajax/init_question_content/id-' + QUESTION_ID, function (response) {
 			$('.question-loader').html(response);
 
